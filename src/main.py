50c06b3e7dabe0308e6cf25f7c9858d109d520e3
Sahil Garg <sahilga@synopsys.com>
2024-11-20 21:37:50 +0530

    python actions poc

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import os
+
+def main():
+    input_num = os.getenv('INPUT_NUM', "0")
+    try:
+        number = int(input_num)
+        square = number ** 2
+        print(f"::set-output name=result::{sqaure}")
+    except ValueError:
+        print("::error::Invalid input, please provide a number")
+
+if __name__ == "__main__":
+    main()
