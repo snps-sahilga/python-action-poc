d3c0d120e83ee613a22581df290af161a83a7ecc
Sahil Garg <sahilga@synopsys.com>
2024-11-21 15:48:45 +0530

    Get Diff for synchronize

--- src/main.py
+++ src/main.py
@@ -37,51 +37,14 @@ def get_pr_details():
 
 def get_diff(owner, repo, pull_number):
     pr = g.get_repo(f"{owner}/{repo}").get_pull(pull_number)
-    print(pr.url)
     headers = {'Authorization': f"token {GITHUB_TOKEN}", 'Accept': 'application/vnd.github.v3.diff'}
-#     headers = {'Accept': 'application/vnd.github.v3.diff'}
-#     response = g._Github__requester.requestRaw("GET", pr.url, headers=headers)
-#     print(response.text)
-#     print(response.status_code)
-#
     response = requests.get(pr.url, headers=headers)
     if response.status_code == 200:
         diff_content = response.text
-        print("Pull request diff:")
-        print(diff_content)
         return diff_content
     else:
         print(f"Failed to get pull request diff: {response.status_code}")
-        print(response.text)
         return
-#     pr_diff_url = f"https://api.github.com/repos/{owner}/{repo}/pulls/{pull_number}"
-
-    # Fetch the pull request diff
-#     try:
-#         pr_response = requests.get(pr_diff_url, headers=headers)
-#
-#         if pr_response.status_code == 200:
-#             pr_data = pr_response.json()
-#             diff_url = pr_data.get('diff_url')
-#
-#             if diff_url:
-#                 diff_response = requests.get(diff_url, headers=headers)
-#                 if diff_response.status_code == 200:
-#                     diff_content = diff_response.text
-#                     print("Pull request diff:")
-#                     print(diff_content)
-#                 else:
-#                     print(f"Failed to get pull request diff: {diff_response.status_code}")
-#                     print(diff_response.text)
-#             else:
-#                 print("No diff URL found in pull request data.")
-#         else:
-#             print(f"Failed to get pull request: {pr_response.status_code}")
-#             print(pr_response.text)
-#             return
-#     except Exception as e:
-#         print(f"Exception occurred while fetching pull request data: {e}")
-#         return
 
 def main():
     pr_details = get_pr_details()
@@ -89,7 +52,16 @@ def main():
         event_data = json.load(f)
     if event_data["action"] == "opened":
         diff = get_diff(pr_details.owner, pr_details.repo, pr_details.pull_number)
-    print(diff)
+    elif event_data["action"] == "synchronize":
+        new_base_sha = event_data["before"]
+        new_head_sha = event_data["after"]
+        repo = g.get_repo(f"{pr_details.owner}/{pr_details.repo}")
+        diff = repo.compare(new_base_sha, new_head_sha).diff
+    if diff == None:
+        print("No Diff Found!!!")
+        return
+    else:
+        print(diff)
     input_num = os.getenv('INPUT_NUM')
     try:
         number = int(input_num)
