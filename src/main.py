29c92637918a490392cd5b2ba0d83bf0ee67f3ec
Sahil Garg <sahilga@synopsys.com>
2024-11-21 18:13:46 +0530

    Analyse Code

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_API_MODEL = os.getenv("OPENAI_API_MODEL")
 OPENAI_API_ENDPOINT = os.getenv("OPENAI_API_ENDPOINT")
 OPENAI_API_VERSION = os.getenv("OPENAI_API_VERSION")
+EXCLUDE = os.getenv("exclude")
 
 g = Github(GITHUB_TOKEN)
 
@@ -51,36 +52,39 @@ def main():
     with open(os.getenv("GITHUB_EVENT_PATH"), "r") as f:
         event_data = json.load(f)
     if event_data["action"] == "opened":
-#         diff = get_diff(pr_details.owner, pr_details.repo, pr_details.pull_number)
-        print("Opened")
+        # diff = get_diff(pr_details.owner, pr_details.repo, pr_details.pull_number)
         pr = g.get_repo(f"{pr_details.owner}/{pr_details.repo}").get_pull(pr_details.pull_number)
         new_base_sha = pr.base.sha
         new_head_sha = pr.head.sha
-        repo = g.get_repo(f"{pr_details.owner}/{pr_details.repo}")
-        diff = repo.compare(new_base_sha, new_head_sha)
     elif event_data["action"] == "synchronize":
-        print("Synchronized")
         new_base_sha = event_data["before"]
         new_head_sha = event_data["after"]
-        repo = g.get_repo(f"{pr_details.owner}/{pr_details.repo}")
-        diff = repo.compare(new_base_sha, new_head_sha)
+    else:
+        print("Unsupported event:", os.getenv("GITHUB_EVENT_NAME"))
+        return
+    repo = g.get_repo(f"{pr_details.owner}/{pr_details.repo}")
+    diff = repo.compare(new_base_sha, new_head_sha)
+
     if diff == None:
         print("No Diff Found!!!")
         return
-    else:
-#         print(diff)
-        print(f"Status of comparison: {diff.status}")
-        print(f"Total commits in the comparison: {diff.total_commits}")
 
-        # Iterate over the files in the comparison
-        for file in diff.files:
-            print(f"Filename: {file.filename}")
-            print(f"Status: {file.status}")
-            print(f"Additions: {file.additions}")
-            print(f"Deletions: {file.deletions}")
-            print(f"Changes: {file.changes}")
-            print(file.patch)
-            print("")
+    exclude_patterns = [s.strip() for s in EXCLUDE.split(",")]
+    filtered_diff = [file for file in diff if not any(fnmatch.fnmatch(file.filename, pattern) for pattern in exclude_patterns)]
+
+    print(f"Status of comparison: {diff.status}")
+    print(f"Total commits in the comparison: {diff.total_commits}")
+
+    # Iterate over the files in the comparison
+    for file in diff.files:
+        print(f"Filename: {file.filename}")
+        print(f"Status: {file.status}")
+        print(f"Additions: {file.additions}")
+        print(f"Deletions: {file.deletions}")
+        print(f"Changes: {file.changes}")
+        print(file.patch)
+        print("")
+
     input_num = os.getenv('INPUT_NUM')
     try:
         number = int(input_num)
