3cf1e7a2882c93ae4158eb7e83c9fb4ff940bd59
Sahil Garg <sahilga@synopsys.com>
2024-11-21 10:33:01 +0530

    Updated action.yml and main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ def main():
     try:
         number = int(input_num)
         square = number ** 2
-        print(f"::set-output name=result::{sqaure}")
+        print(f"::set-output name=result::{square}")
     except ValueError:
         print("::error::Invalid input, please provide a number")
 
