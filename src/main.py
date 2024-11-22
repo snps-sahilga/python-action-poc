70776bec1a6885258dc86c43fc7bc501c90dfd26
Sahil Garg <sahilga@synopsys.com>
2024-11-22 15:16:57 +0530

    Create Prompt

--- src/main.py
+++ src/main.py
@@ -137,8 +137,7 @@ def main():
     patches = PatchSet(diff)
     for patch in patches:
         for hunk in patch:
-            print(section_header)
-            print(lines)
+            print(hunk)
 
 #     exclude_patterns = [s.strip() for s in EXCLUDE.split(",")]
 #     filtered_diff = [file for file in diff.files if not any(fnmatch.fnmatch(file.filename, pattern) for pattern in exclude_patterns)]
